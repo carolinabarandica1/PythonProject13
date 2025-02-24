cec780ebef06bd077f4e9684e93e93597207719b
carolinabarandica <carolinabarandica1@gmail.com>
2025-02-24 15:14:22 +0100

    new commit

---
+++ dictionart.py
@@ -0,0 +1,20 @@
+movies = {
+
+    "Terminator": {"director": "James Cameron", "genre": "Action"},
+    "Mad Max: Fury Road": {"director": "George Miller", "genre": "Action"},
+    "The Matrix": {"director": "Lana & Lilly Wachowski", "genre": "Action"},
+    "The Dark Knight": {"director": "Christopher Nolan", "genre": "Action"},
+    "The Lord of the Rings": {"director": "Peter Jackson", "genre": "Action"},
+    "Top Gun": {"director": "Tony Scott", "genre": "Action"},
+    "Mission: Impossible": {"director": "Brian De Palma", "genre": "Action"},
+    "The Avengers": {"director": "Joss Whedon", "genre": "Action"},
+
+    "Superbad": {"director": "Greg Mottola", "genre": "Comedy"},
+    "Step Brothers": {"director": "Adam McKay", "genre": "Comedy"},
+    "Dumb and Dumber": {"director": "Peter Farrelly", "genre": "Comedy"},
+    "Anchorman": {"director": "Adam McKay", "genre": "Comedy"},
+    "The Hangover": {"director": "Todd Phillips", "genre": "Comedy"},
+    "Tropic Thunder": {"director": "Ben Stiller", "genre": "Comedy"},
+    "Zoolander": {"director": "Ben Stiller", "genre": "Comedy"},
+    "Ferris Bueller's Day Off": {"director": "John Hughes", "genre": "Comedy"}
+}
